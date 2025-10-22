62a16a8e4dcacffe0738577ef95a51b471bace57
ABDUL SUBHAN <ABDULSUBHAN2@BAGH.MTBC.COM>
2025-10-22 16:23:26 +0500

    Updated

--- Backend/server.js
+++ Backend/server.js
@@ -22,16 +22,15 @@ const app = express();
 //Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(
-// 	cors({
-// 		origin: [process.env.FRONTEND_URL],
-// 		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-// 		allowedHeaders: ["Content-Type", "Authorization"],
-// 		credentials: true,
-// 	})
-// );
-app.use(cors());
-//DB config
+app.use(
+	cors({
+		origin: [process.env.FRONTEND_URL],
+		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+		credentials: true,
+	})
+);
+
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.connectionstring, {});
